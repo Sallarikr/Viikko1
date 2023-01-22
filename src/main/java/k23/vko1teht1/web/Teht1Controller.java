2895c1a18db37157cbefac186497ec29fcf2ca5b
Salla Komppa <salla.halminen@hotmail.com>
2023-01-22 20:26:41 +0200

    Lisätty tehtävä 2

--- src/main/java/k23/vko1teht1/web/Teht1Controller.java
+++ src/main/java/k23/vko1teht1/web/Teht1Controller.java
@@ -2,6 +2,7 @@ package k23.vko1teht1.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,5 +18,12 @@ public class Teht1Controller {
 	@ResponseBody
 	public String returnContactPage() {
 		return "This is the contact page";
+	}
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String sayHello(@RequestParam(name="name") String name, 
+			@RequestParam(name="location") String location) {
+		return "Welcome to the " + location + " " + name + "!";
 	}
 }
